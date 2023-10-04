cc098de20c065c777cf1b477fcb9a90f3afd9561
VictorS67 <185000048@qq.com>
2023-10-04 11:34:27 -0400

    rename codetester to codeta

--- code-ta/code_ta/wsgi.py
+++ code-ta/code_ta/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for code_tester project.
+WSGI config for code_ta project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_tester.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_ta.settings')
 
 application = get_wsgi_application()
